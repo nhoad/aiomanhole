22123325d432482a2425055bed7ae0a4fdc5718e
Nathan Hoad <nathan@hoad.io>
2022-01-23 10:29:49 -0500

    Fix version specifier for 3.9.

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ settings = {
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
     ],
     "packages": ["aiomanhole"],
